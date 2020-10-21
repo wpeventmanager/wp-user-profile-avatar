007c0242dcbb2657e2d6d27e534daf42b6623267
Pooja Sahu <71404357+sahupooja93@users.noreply.github.com>
2020-10-21 18:38:40 +0530

    File Error Resolved

--- templates/comments-settings-page.php
+++ templates/comments-settings-page.php
@@ -1,25 +1,10 @@
 <?php
 /**
- * Comment Setting page.
+ * Setting page.
 */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
-}
-
-?>
-<div class="wrap">
-<h1><?php _e( 'disable Comments', 'disable-comments' ); ?></h1>
-<?php
-
-global $wpdb;
-$comments_count = $wpdb->get_var( "SELECT count(comment_id) from $wpdb->comments" );
-if ( $comments_count <= 0 ) {?>
-
-<p><strong><?php _e( 'No comments available for disable.', 'disable-comments' ); ?></strong></p>
-</div>
-<?php
-	return;
 }
 
 function update_options($options) {
@@ -50,61 +35,28 @@ $types = get_post_types( $typeargs, 'objects' );
 // 	}
 // }
 
-
-if ( isset( $_POST['submit'] ) && isset( $_POST['mode'] ) ) {
+if ( isset( $_POST['submit'] ) ) {
 	check_admin_referer( 'disable-comments-admin' );
+	$options['remove_everywhere'] = ( $_POST['mode'] == 'remove_everywhere' );
 
-	if ( $_POST['mode'] == 'remove_everywhere' ) {
-		if ( $wpdb->query( "TRUNCATE $wpdb->commentmeta" ) != false ) {
-			if ( $wpdb->query( "TRUNCATE $wpdb->comments" ) != false ) {
-				$wpdb->query( "UPDATE $wpdb->posts SET comment_count = 0 WHERE post_author != 0" );
-				$wpdb->query( "OPTIMIZE TABLE $wpdb->commentmeta" );
-				$wpdb->query( "OPTIMIZE TABLE $wpdb->comments" );
-				echo "<p style='color:green'><strong>" . __( 'All comments have been disable.', 'disable-comments' ) . '</strong></p>';
-			} else {
-				echo "<p style='color:red'><strong>" . __( 'Internal error occured. Please try again later.', 'disable-comments' ) . '</strong></p>';
-			}
-		} else {
-			echo "<p style='color:red'><strong>" . __( 'Internal error occured. Please try again later.', 'disable-comments' ) . '</strong></p>';
-		}
+	if ( $options['remove_everywhere'] ) {
+		$disabled_post_types = array_keys( $types );
 	} else {
 		$disabled_post_types = empty( $_POST['disabled_types'] ) ? array() : (array) $_POST['disabled_types'];
-		$disabled_post_types = array_intersect( $disabled_post_types, array_keys( $types ) );
-
-		// Extra custom post types.
-		if ( ! empty( $_POST['$disabled_extra_post_types'] ) ) {
-			$disabled_extra_post_types = array_filter( array_map( 'sanitize_key', explode( ',', $_POST['$disabled_extra_post_types'] ) ) );
-			$disabled_extra_post_types = array_diff( $disabled_extra_post_types, array_keys( $types ) );    // Make sure we don't double up builtins.
-			$disabled_post_types      = array_merge( $disabled_post_types , $disabled_extra_post_types );
-		}
-
-		if ( ! empty( $disabled_post_types ) ) {
-			// Loop through post_types and remove comments/meta and set posts comment_count to 0.
-			foreach ( $disabled_post_types as $disabled_post_type ) {
-				$wpdb->query( "DELETE cmeta FROM $wpdb->commentmeta cmeta INNER JOIN $wpdb->comments comments ON cmeta.comment_id=comments.comment_ID INNER JOIN $wpdb->posts posts ON comments.comment_post_ID=posts.ID WHERE posts.post_type = '$disabled_post_type'" );
-				$wpdb->query( "DELETE comments FROM $wpdb->comments comments INNER JOIN $wpdb->posts posts ON comments.comment_post_ID=posts.ID WHERE posts.post_type = '$disabled_post_type'" );
-				$wpdb->query( "UPDATE $wpdb->posts SET comment_count = 0 WHERE post_author != 0 AND post_type = '$disabled_post_type'" );
-
-				$post_type_object = get_post_type_object( $disabled_post_type );
-				$post_type_label  = $post_type_object ? $post_type_object->labels->name : $disabled_post_type;
-				echo "<p style='color:green'><strong>" . sprintf( __( 'All comments have been disable for %s.', 'disable-comments' ), $post_type_label ) . '</strong></p>';
-			}
-
-			$wpdb->query( "OPTIMIZE TABLE $wpdb->commentmeta" );
-			$wpdb->query( "OPTIMIZE TABLE $wpdb->comments" );
-
-			echo "<h4 style='color:green'><strong>" . __( 'Comment Deletion Complete', 'disable-comments' ) . '</strong></h4>';
-		}
 	}
 
-	$comments_count = $wpdb->get_var( "SELECT count(comment_id) from $wpdb->comments" );
-	if ( $comments_count <= 0 ) {
-		?>
-		<p><strong><?php _e( 'No comments available for deletion.', 'disable-comments' ); ?></strong></p>
-		</div>
-		<?php
-		return;
+	$disabled_post_types = array_intersect( $disabled_post_types, array_keys( $types ) );
+
+	$options['disabled_post_types'] = $disabled_post_types;
+
+	// Extra custom post types.
+	if ( ! empty( $_POST['extra_post_types'] ) ) {
+		$extra_post_types                  = array_filter( array_map( 'sanitize_key', explode( ',', $_POST['extra_post_types'] ) ) );
+		$options['extra_post_types'] = array_diff( $extra_post_types, array_keys( $types ) ); // Make sure we don't double up builtins.
 	}
+
+	update_options($options);
+	echo '<div id="message" class="updated"><p>' . __( 'Options updated. Changes to the Admin Menu and Admin Bar will not appear until you leave or reload this page.', 'disable-comments' ) .'</p></div>';
 }
 ?>
 <div class="wrap">
@@ -112,15 +64,20 @@ if ( isset( $_POST['submit'] ) && isset( $_POST['mode'] ) ) {
 
 <form action="" method="post" id="disable-comments">
 <ul>
-<li><label for="remove_everywhere"><input type="radio" id="remove_everywhere" name="mode" value="remove_everywhere" <?php checked( $options['remove_everywhere'] ); ?> /> <strong><?php _e( 'Everywhere', 'disable-comments' ); ?></strong>: <?php _e( 'Disable all comment-related controls and settings in WordPress.', 'disable-comments' ); ?></label>
+<li><label for="remove_everywhere"><input type="radio" id="remove_everywhere" name="mode" value="remove_everywhere" <?php checked(isset( $options['remove_everywhere'] ) ); ?> /> <strong><?php _e( 'Everywhere', 'disable-comments' ); ?></strong>: <?php _e( 'Disable all comment-related controls and settings in WordPress.', 'disable-comments' ); ?></label>
 	<p class="indent"><?php printf( __( '%1$s: This option is global and will affect your entire site. Use it only if you want to disable comments <em>everywhere</em>. A complete description of what this option does is <a href="%2$s" target="_blank">available here</a>.', 'disable-comments' ), '<strong style="color: #900">' . __( 'Warning', 'disable-comments' ) . '</strong>', 'https://wordpress.org/plugins/disable-comments/other_notes/' ); ?></p>
 </li>
 <li><label for="selected_types"><input type="radio" id="selected_types" name="mode" value="selected_types" <?php checked( ! $options['remove_everywhere'] ); ?> /> <strong><?php _e( 'On certain post types', 'disable-comments' ); ?></strong>:</label>
 	<p></p>
 	<ul class="indent" id="listoftypes">
-		<?php
+	<?php
+		if(isset($options['disabled_post_types']) && is_array($options['disabled_post_types'])){
+			$disabled_post_types = $options['disabled_post_types'];
+		}else{
+			$disabled_post_types = [];
+		}
 		foreach ( $types as $k => $v ) {
-	echo "<li><label for='post-type-$k'><input type='checkbox' name='disabled_types[]' value='$k' " . checked( $options['disabled_post_types'], true, false ) . " id='post-type-$k'> {$v->labels->name}</label></li>";}
+			echo "<li><label for='post-type-$k'><input type='checkbox' name='disabled_types[]' value='$k' " . checked( in_array( $k,  $disabled_post_types), true, false ) . " id='post-type-$k'> {$v->labels->name}</label></li>";}
 		?>
 	</ul>
 	
@@ -129,7 +86,6 @@ if ( isset( $_POST['submit'] ) && isset( $_POST['mode'] ) ) {
 </ul>
 
 <?php wp_nonce_field( 'disable-comments-admin' ); ?>
-<h4><?php _e( 'Total Comments:', 'disable-comments' ); ?> <?php echo $comments_count; ?></h4>
 <p class="submit"><input class="button-primary" type="submit" name="submit" value="<?php _e( 'Save Changes', 'disable-comments' ); ?>"></p>
 </form>
 </div>
