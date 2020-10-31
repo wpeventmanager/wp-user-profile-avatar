998b9cbb81767a0c28fc288efa6806e03adfafe4
Pooja Sahu <71404357+sahupooja93@users.noreply.github.com>
2020-10-31 19:51:41 +0530

    Issue resolve Restrict access to Media Library
    Subscribers and other users can only see/select own  Media Library ,They Can't able to access Administrator Media Library.

--- wp-user-profile-avatar-functions.php
+++ wp-user-profile-avatar-functions.php
@@ -360,3 +360,15 @@ if ( ! function_exists( 'get_wpupa_image_link_to' ) ) {
 		) );
 	}
 }
+// Restrict access to Media Library (users can only see/select own media)
+  
+add_filter( 'ajax_query_attachments_args', 'wpb_show_current_user_attachments' );
+ 
+function wpb_show_current_user_attachments( $query ) {
+    $user_id = get_current_user_id();
+    if ( $user_id && !current_user_can('activate_plugins') && !current_user_can('edit_others_posts
+') ) {
+        $query['author'] = $user_id;
+    }
+    return $query;
+} 
