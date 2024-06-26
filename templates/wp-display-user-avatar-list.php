92fdd9112359860e15c4786f1841b3e487264d63
Roshani Savsani <roshani.koradiya264@gmail.com>
2024-06-26 18:24:48 +0530

    #21 Changed avatar list template
    List the avatar according to blogs attribute if it is available in shortcode parameter

--- templates/wp-display-user-avatar-list.php
+++ templates/wp-display-user-avatar-list.php
@@ -2,68 +2,76 @@
 /**
  * Display user avatar list template
  */
-if (!defined('ABSPATH')) {
+if ( !defined( 'ABSPATH' ) ) {
     exit;
 }
 
 ?>
 
-<div class="user-avatar-list" style="text-align: <?php echo esc_attr($atts['align']); ?>;">
-    <?php if (!empty($user_avatars)) {
-		$avatar_counter = 0;
-        foreach ($user_avatars as $user) {  ?>
-            <div class="user-avatar" style="display: inline-block; margin: 10px;text-align:center;">
-                <?php if (!empty($user['avatar_url'])) { ?>
-                    <img src="<?php echo esc_url($user['avatar_url']); ?>" alt="<?php echo esc_attr($user['display_name']); ?>" style="border-radius: <?php echo esc_attr($atts['border_radius']); ?>px; width: <?php echo esc_attr($atts['avatar_size']); ?>px; height: <?php echo esc_attr($atts['avatar_size']); ?>px;"/><br>
-                <?php } ?>
-				<?php $author_page_url = get_author_posts_url($user['ID']);?>
-                <?php if (!empty($user['show_name'])) { ?>
-                    <span class="user-name"><a href="<?php echo $user['avatar_url'];?>"><?php echo esc_html($user['display_name']); ?></a></span>
-                <?php } ?>
-				
-				<?php if (!empty($user['post_count'])) {
-                    echo '<span class="user-postcount">('.$user['post_count'].')</span><br>';
-                } else {
-                    echo '<span class="user-postcount">(0)</span><br>';
-                } ?>
+<div class="user-avatar-list" style="text-align: <?php echo esc_attr($atts['align'] ); ?>;">
+    <?php if ( !empty( $user_avatars ) ) {
+        foreach ( $user_avatars as $blog_id => $users ) { 
+            if ( $blog_id > 0 ) { ?>
+                <h2><?php echo esc_html( $users[0]['blog_name'] ); ?></h2>
+            <?php }
+            foreach ( $users as $user ) { ?>
+                <div class="user-avatar" style="display: inline-block; margin: 10px;text-align:center;">
+                    <?php if ( !empty( $user['avatar_url'] ) ) { ?>
+                        <img src="<?php echo esc_url( $user['avatar_url'] ); ?>" alt="<?php echo esc_attr( $user['display_name'] ); ?>" style="border-radius: <?php echo esc_attr( $atts['border_radius'] ); ?>px; width: <?php echo esc_attr( $atts['avatar_size'] ); ?>px; height: <?php echo esc_attr( $atts['avatar_size'] ); ?>px;"/><br>
+                    <?php } ?>
 
-                <?php if (!empty($user['biography'])) { ?>
-                    <span class="user-biography"><?php echo esc_html($user['biography']); ?></span>
-                <?php } ?>
+                    <?php if ( !empty( $atts['link_to_authorpage'] ) && $atts['link_to_authorpage'] === 'true' ) { ?>
+                        <a href="<?php echo get_author_posts_url( $user['ID'] ); ?>">
+                    <?php } ?>
 
-                <?php if (!empty($user['user_link'])) {
-                    switch ($user['user_link']) {
-                        case 'website':
-                            echo '<p class="user-link"><a href="User website URL">Visit Website</a></p>';
-                            break;
-                        case 'authorpage':
-                            $author_page_url = get_author_posts_url($user['ID']);
-                            echo '<p class="user-link"><a href="' . $author_page_url . '">View Profile</a></p>';
-                            break;
-                        default:
-                            break;
-                    }
-                } ?>
-            </div>
-        <?php }
+                    <?php if ( !empty( $user['show_name'] ) && $user['show_name'] ) { ?>
+                        <span class="user-name"><?php echo esc_html( $user['display_name'] ); ?></span>
+                    <?php } ?>
+
+                    <?php if ( !empty( $atts['link_to_authorpage'] ) && $atts['link_to_authorpage'] === 'true' ) { ?>
+                        </a><br>
+                    <?php } ?>
+
+                    <?php if ( !empty( $user['post_count'] ) ) { ?>
+                        <span class="user-postcount">( <?php echo $user['post_count']; ?> )</span><br>
+                    <?php }else{?>
+						<span class="user-postcount">( 0 )</span><br>
+					<?php } 
+
+                     if ( !empty( $user['biography'] ) ) { ?>
+                        <span class="user-biography"><?php echo esc_html( $user['biography'] ); ?></span>
+                    <?php } ?>
+
+                    <?php if ( !empty( $user['user_link'] ) ) {
+                        switch ( $user['user_link'] ) {
+                            case 'website':
+                                echo '<p class="user-link"><a href="User website URL">Visit Website</a></p>';
+                                break;
+                            case 'authorpage':
+                                $author_page_url = get_author_posts_url( $user['ID'] );
+                                echo '<p class="user-link"><a href="' . $author_page_url . '">View Profile</a></p>';
+                                break;
+                            default:
+                                break;
+                        }
+                    } ?>
+                </div>
+            <?php }
+        }
     } else { ?>
         <p>No avatars available</p>
     <?php } ?>
-	
-	<div class="pagination" style="text-align:center;">
-		<?php
-		
-		echo paginate_links(array(
-			'base' => get_pagenum_link(1) . '%_%',
-			'format' => 'page/%#%/',
-			'current' => max(1, get_query_var('paged', 1)),
-			'total' => $total_pages,
-			'prev_text' => '«',
+
+     <div class="pagination" style="text-align: center;">
+        <?php
+        echo paginate_links(array(
+            'base' => get_pagenum_link(1) . '%_%',
+            'format' => 'page/%#%/',
+            'current' => max(1, get_query_var('paged', 1)),
+            'total' => $total_pages,
+            'prev_text' => '«',
             'next_text' => '»',
-		));
-		?>
-		
-	</div>
+        ));
+        ?>
+    </div>
 </div>
-
-
