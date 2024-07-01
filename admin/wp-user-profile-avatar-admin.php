3f788143d8b33930ec5a8ebc70e424d4705aa147
Roshani <roshani.koradiya264@gmail.com>
2024-07-01 18:18:26 +0530

    #122 Changed in Code in User Avatar Setting  Page
    Disable code that is conflict with user avatar profile setting page's fields.

--- admin/wp-user-profile-avatar-admin.php
+++ admin/wp-user-profile-avatar-admin.php
@@ -206,9 +206,9 @@ class WPUPA_Admin {
 
             $wpupa_disable_gravatar = ! empty( $_POST['wpupa-disable-gravatar'] ) ? sanitize_text_field( $_POST['wpupa-disable-gravatar'] ) : '';
 
-            update_option( 'wpupa_tinymce', $wpupa_tinymce );
-            update_option( 'wpupa_allow_upload', $wpupa_allow_upload );
-            update_option( 'wpupa_disable_gravatar', $wpupa_disable_gravatar );
+            // update_option( 'wpupa_tinymce', $wpupa_tinymce );
+            //update_option( 'wpupa_allow_upload', $wpupa_allow_upload );
+            //update_option( 'wpupa_disable_gravatar', $wpupa_disable_gravatar );
 
             if ( ! empty( $wpupaattachmentid ) || ! empty( $wpupa_url ) ) {
                 update_user_meta( $user_id, '_wpupa_default', sanitize_text_field( 'wp_user_profile_avatar' ) );
