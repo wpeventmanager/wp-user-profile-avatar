5687a13371c5752df2c0e9d362b8a04c9efbff60
Roshani <roshani.koradiya264@gmail.com>
2024-07-19 10:20:29 +0530

    #123 make string translatable

--- templates/wp-display-user-avatar-list.php
+++ templates/wp-display-user-avatar-list.php
@@ -54,7 +54,7 @@ if ( !defined( 'ABSPATH' ) ) {
             <?php }
         }
     } else { ?>
-        <p>No avatars available</p>
+        <p><?php echo esc_html__( 'No avatars available' ); ?></p>
     <?php } ?>
 
     <div class="pagination" style="text-align: center;">
